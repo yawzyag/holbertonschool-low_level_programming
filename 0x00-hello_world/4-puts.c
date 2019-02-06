00f4ca68009e0b6917d69334201eb33c895f026e
yesid.dev <yesid.yag@gmail.com>
2019-02-06 17:54:52 +0000

    print whit puts, correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {
-char str1[15];
-strcpy(str1, "Programming is like building a multilingual puzzle\n");
-puts(str1);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
