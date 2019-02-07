6de2c9f9d0f615f99354087ac22f3e77024f3892
Yesid.dev <yesid.yag@gmail.com>
2019-02-07 17:12:44 +0000

    adding comentary headers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,13 +3,18 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-/* your code goes there */
+        /* your code goes there */
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
