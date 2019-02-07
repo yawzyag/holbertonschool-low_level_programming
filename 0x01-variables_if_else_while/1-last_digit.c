533e9048f63bdacb4b142edb21cdc625eae6e75b
Yesid.dev <yesid.yag@gmail.com>
2019-02-07 17:18:24 +0000

    adding comentary headers 1file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,15 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int n, lastDigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	lastDigit = n % 10;
 	if (lastDigit > 5)
 	{
