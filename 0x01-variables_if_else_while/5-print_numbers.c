69042197c782a9a1f999bd43f04cd4e114b9fe4c
Yesid.dev <yesid.yag@gmail.com>
2019-02-07 19:29:30 +0000

    print single numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int var;
+
+	for (var = 0; var < 10; var++)
+	{
+		printf("%d", var);
+	}
+	printf("\n");
+	return (0);
+}
