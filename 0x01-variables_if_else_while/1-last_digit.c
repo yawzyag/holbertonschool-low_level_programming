017cf72482169da2f12987b00ced2d611a2e4c07
Yesid.dev <yesid.yag@gmail.com>
2019-02-07 17:07:32 +0000

    alphabet in lower case

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,8 @@ int main(void)
 	lastDigit = n % 10;
 	if (lastDigit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n",
+		       n, lastDigit);
 	}
 	else if (lastDigit == 0)
 	{
@@ -21,7 +22,8 @@ int main(void)
 	}
 	else if (lastDigit < 6)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+		       n, lastDigit);
 	}
 	return (0);
 }
