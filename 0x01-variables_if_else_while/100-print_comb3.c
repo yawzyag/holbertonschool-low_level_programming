80f879e6046cf8614c3a1fb7ba3e9358f4e2296b
Yesid <yesid.dev93@gmail.com>
2019-02-10 20:55:16 +0000

    add changes

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,28 +7,24 @@
  */
 int main(void)
 {
-	int num1;
-	int num2;
+	int num1, num2;
 
-	for (num1 = '0'; num1 <= '9' ; num1++)
+	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 = '0'; num2 <= '9'; num2++)
+		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
-			if ((num1 != num2) && (num1 < num2))
+			putchar((num1) + '0');
+			putchar((num2) + '0');
+			if (num1 < 8)
 			{
-				putchar(num1);
-				putchar(num2);
-				if ((num1 == '8' && num2 == '9'))
-				{
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
+			}
+			else
+			{
+				putchar('\n');
 			}
 		}
 	}
-	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,34 +7,26 @@
  */
 int main(void)
 {
-	int num1;
-	int num2;
-	int num3;
+	int num1, num2, num3;
 
-	for (num1 = '0'; num1 <= '9'; num1++)
+	for (num1 = 0; num1 < 10; num1++)
 	{
-		for (num2 = '0'; num2 <= '9'; num2++)
+		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
-			for (num3 = '0'; num3 <= '9'; num3++)
+			for(num3 = num2 + 1; num3 < 10; num3++)
 			{
-			if ((num1 != num2) && (num1 < num2))
-			{
-				if ((num2 != num3) && (num2 < num3))
-				{
-				putchar(num1);
-				putchar(num2);
-				putchar(num3);
-				if ((num1 == '7' && num2 == '8') && num3 == '9')
-				{
-					putchar('\n');
-				}
-				else
+				putchar((num1) + '0');
+				putchar((num2) + '0');
+				putchar((num3) + '0');
+				if (num1 < 7)
 				{
 					putchar(',');
 					putchar(' ');
 				}
+				else
+				{
+					putchar('\n');
 				}
-			}
 			}
 		}
 	}
