2e6af59d5252491f533bed05ff629909fe8bc675
Yesid.dev <yesid.yag@gmail.com>
2019-02-11 20:56:03 +0000

    print holberton whit _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char var[] = "Holberton";
+	int i;
+
+	for(i = 0; i < 9; i ++)
+	{
+		_putchar(var[i]);
+	}
+	_putchar('\n');
+	return(0);
+}
