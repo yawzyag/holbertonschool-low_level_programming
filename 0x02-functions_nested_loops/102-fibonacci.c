82f1f868c40c0d9a9a52582343d8707a8764d4bc
Yesid.dev <yesid.yag@gmail.com>
2019-02-12 20:49:46 +0000

    50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	long int num1 = 1, num2 = 2, fib;
+
+	for (i = 1; i < 50; i++)
+	{
+		if (i > 1 && i < 50)
+		{
+			printf(", ");
+		}
+		fib = num1;
+		num1 = num2;
+		num2 = fib + num1;
+		printf("%ld", fib);
+	}
+	printf("\n");
+	return (0);
+}
