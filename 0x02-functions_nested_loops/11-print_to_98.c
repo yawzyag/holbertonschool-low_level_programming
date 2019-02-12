347e3711fcc5540ecc1645cf9dd8176ebd911580
Yesid.dev <yesid.yag@gmail.com>
2019-02-12 15:29:11 +0000

    numbers to 98 using standart library

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* print_to_98 - show the absolute value of a number
+* @n: number to check
+*
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
