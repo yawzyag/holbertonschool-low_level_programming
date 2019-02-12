d68183c36b81586559927960bf5808035aa6eb8e
Yesid.dev <yesid.yag@gmail.com>
2019-02-12 19:54:38 +0000

    natural numbers sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int num = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			num = num + i;
+		}
+	}
+	printf("the sum is: %d\n", num);
+	return (0);
+}
