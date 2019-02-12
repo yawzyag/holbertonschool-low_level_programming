e078057aa932d21a87922c5b37f4f57bb424a4ce
Yesid <yesid.dev93@gmail.com>
2019-02-12 04:29:24 +0000

    table of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+* times_table - show a table whit 9 multiplication
+*
+* Return: The table
+*/
+void times_table(void)
+{
+	int var, i, c;
+
+	for (var = 0; var < 10 ; var++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			c = i * var;
+			if (c >= 10)
+			{
+				if (i > 0 && i < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+			else if (c < 10)
+			{
+				if (i > 0 && i < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(c + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
