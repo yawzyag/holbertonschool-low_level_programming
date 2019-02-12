53a507dad2502291490fca33c8803cf9484db292
Yesid.dev <yesid.yag@gmail.com>
2019-02-12 17:28:40 +0000

    adding betty changes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,15 @@
 #include "holberton.h"
 
+/**
+ * print_times_table - show the table of a number
+ * @n: number to check
+ *
+ */
 void print_times_table(int n)
 {
 	int var, i, c;
 
-	if(n > 0 && n < 15)
+	if (n > 0 && n < 15)
 	{
 		for (var = 0; var <= n; var++)
 		{
@@ -14,24 +19,24 @@ void print_times_table(int n)
 				if (c >= 100)
 				{
 					if (i > 0 && i < 15)
-                                        {
-                                                _putchar(',');
-                                                _putchar(' ');
-                                        }
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
 					_putchar((c / 100) + '0');
 					_putchar(((c % 100) / 10) + '0');
 					_putchar((c % 10) + '0');
 				}
-				else if(c >= 10)
+				else if (c >= 10)
 				{
 					if (i > 0 && i < 15)
-                                        {
-                                                _putchar(',');
-                                                _putchar(' ');
+					{
+						_putchar(',');
 						_putchar(' ');
-                                        }
-                                        _putchar((c / 10) + '0');
-                                        _putchar((c % 10) + '0');
+						_putchar(' ');
+					}
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
 				}
 				else
 				{
