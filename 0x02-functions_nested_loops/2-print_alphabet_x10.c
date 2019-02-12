b9b269f6617cd84740e1c223655c3f4871dad4ec
Yesid.dev <yesid.yag@gmail.com>
2019-02-12 14:24:36 +0000

    alphabte correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,28 +10,12 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		print_alphabet();
+		char var;
+
+		for (var = 'a'; var <= 'z'; var++)
+		{
+			_putchar (var);
+		}
 		_putchar('\n');
 	}
-}
-
-
-/**
-* print_alphabet - Prints "a to z"
-*
-* This is a longer description.
-* Don't forget that a line should not exceed 80 characters.
-* But you're totally free to use several lines to properly
-* describe your function
-* Return: 0
-*/
-int print_alphabet(void)
-{
-	char var;
-
-	for (var = 'a'; var <= 'z'; var++)
-	{
-		_putchar (var);
-	}
-	return (0);
 }
