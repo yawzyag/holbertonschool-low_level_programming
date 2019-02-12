015c29e1e56764a18ce8a1978daafb63ed00885c
Yesid.dev <yesid.yag@gmail.com>
2019-02-12 14:32:33 +0000

    adding changes to last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,10 @@
 */
 int print_last_digit(int num)
 {
-	int var;
-
 	if (num < 0)
 	{
 		num = -num;
 	}
-	var = num % 10;
-	_putchar('0' + var);
-	return (var);
+	_putchar('0' + (num % 10));
+	return (num % 10);
 }
