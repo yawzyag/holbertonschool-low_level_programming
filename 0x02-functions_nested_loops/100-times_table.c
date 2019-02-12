4a5d5517feb922357649e71d5341753d2a14a7e6
Yesid.dev <yesid.yag@gmail.com>
2019-02-12 17:23:15 +0000

    print tables of a certain number

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+void print_times_table(int n)
+{
+	int var, i, c;
+
+	if(n > 0 && n < 15)
+	{
+		for (var = 0; var <= n; var++)
+		{
+			for (i = 0; i <= n; i++)
+			{
+				c = i * var;
+				if (c >= 100)
+				{
+					if (i > 0 && i < 15)
+                                        {
+                                                _putchar(',');
+                                                _putchar(' ');
+                                        }
+					_putchar((c / 100) + '0');
+					_putchar(((c % 100) / 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else if(c >= 10)
+				{
+					if (i > 0 && i < 15)
+                                        {
+                                                _putchar(',');
+                                                _putchar(' ');
+						_putchar(' ');
+                                        }
+                                        _putchar((c / 10) + '0');
+                                        _putchar((c % 10) + '0');
+				}
+				else
+				{
+					if (i > 0 && i < 15)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(c + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
