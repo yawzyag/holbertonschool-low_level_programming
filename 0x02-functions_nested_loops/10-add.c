7529676fd03fd9d1594f83595e32e292a326feaf
Yesid.dev <yesid.yag@gmail.com>
2019-02-12 14:52:17 +0000

    betty changes

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ * add - show the absolute value of a number
+ * @num1: number1
+ * @num2: number2
+ *
+ * Return: number1 + number2
+ */
 int add(int num1, int num2)
 {
 	return (num1 + num2);
