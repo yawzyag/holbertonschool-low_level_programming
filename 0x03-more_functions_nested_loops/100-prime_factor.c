4fd4140c0bbb4b0aabdded541e6a77411a007d27
Yesid.dev <yesid.yag@gmail.com>
2019-02-14 23:19:05 +0000

    max prime factor

---
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	long i;
+	long num = 612852475143;
+
+	for(i = 2; num > i; i++)
+	{
+		if (num % i == 0)
+		{
+			num = num / i;
+			i = 2;
+		}
+	}
+	printf("%li\n", i);
+	return(0);
+}
