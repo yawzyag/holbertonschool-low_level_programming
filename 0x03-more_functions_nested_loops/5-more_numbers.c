63cda365d3ffd63507f1a1c1feea1617dc4deb75
Yesid.dev <yesid.yag@gmail.com>
2019-02-14 19:56:22 +0000

    0 to 14

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - Prints "a to z" 10 times
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int num;
+		for (num = 0; num <= 14; num++)
+		{
+			if(num > 9)
+			{
+				_putchar ((num / 10) + '0');
+			}
+			_putchar ((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
