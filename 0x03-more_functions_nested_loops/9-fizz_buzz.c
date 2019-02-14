d9f40d55855bcb41fcc6299e67d2ba449b7c4bdb
Yesid.dev <yesid.yag@gmail.com>
2019-02-14 21:13:21 +0000

    Fizz Buzz challenge correction 2

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
