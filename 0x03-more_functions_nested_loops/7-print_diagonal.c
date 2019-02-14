80f3dc0e4f531a319cdb3855ba495383eb2142b2
Yesid.dev <yesid.yag@gmail.com>
2019-02-14 20:51:41 +0000

    print diagonal number of lines

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - show a table whit 9 multiplication
+ * @n: number of lines
+ * description: funtion to print number of lines on diagonal
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int h, i;
+
+	if (n > 0)
+	{
+		for (h = 0; h < n; h++)
+		{
+			for (i = 0; i <= h; i++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
