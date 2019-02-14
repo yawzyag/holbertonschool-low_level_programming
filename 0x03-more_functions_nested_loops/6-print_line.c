43c11905329bf77f415798fb6847c36f6094fe59
Yesid.dev <yesid.yag@gmail.com>
2019-02-14 20:05:24 +0000

    print _

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_line - show a table whit 9 multiplication
+ *@n: variable to print lines
+ *
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
