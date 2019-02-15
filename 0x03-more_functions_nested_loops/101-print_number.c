9cb7afbac04913ef2d02a39e1ec4172c256c7e5c
Yesid.dev <yesid.yag@gmail.com>
2019-02-15 16:04:43 +0000

    changing variable typw

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -7,14 +7,17 @@
  */
 void print_number(int n)
 {
+	unsigned int m;
+
+	m = n;
 	if (n < 0)
 	{
-		n = -n;
+		m = -n;
 		_putchar('-');
 	}
 	if (n / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(m / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((m % 10) + '0');
 }
