b78961d54e2f6e1d863ba34d91d6ffdbf92745c3
Yesid <yesid.dev93@gmail.com>
2019-02-15 01:28:56 +0000

    program to print a number using putchar

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_number - show a number whit putchar
+ *@n: variable to print number
+ *
+ */
+void print_number(int n)
+{
+	if (n < 0) {
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
