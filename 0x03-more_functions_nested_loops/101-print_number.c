4d0ecf2a150d980916b4d78294fe6ceb3aac0c77
Yesid <yesid.dev93@gmail.com>
2019-02-15 03:31:29 +0000

    adding changes

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_number - show a number whit putchar
+ *@n: variable to print number
+ *
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
