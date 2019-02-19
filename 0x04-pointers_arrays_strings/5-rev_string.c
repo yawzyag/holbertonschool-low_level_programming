f208f81cba5e328e7a473d02f05db085efac6121
Yesid <yesid.dev93@gmail.com>
2019-02-19 01:08:21 +0000

    adding changes

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -8,5 +8,31 @@
  */
 void rev_string(char *s)
 {
-	
+	int i, j, length, c;
+	char *begin, *end, temp;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		s++;
+	}
+	length = i;
+	for (j = i; j > 0; j--)
+	{
+		s--;
+	}
+	begin = s;
+	end = s;
+	for (c = 0; c < length - 1; c++)
+	{
+		end++;
+	}
+	for (c = 0; c < length / 2; c++)
+	{
+		temp = *end;
+		*end = *begin;
+		*begin = temp;
+
+		begin++;
+		end--;
+	}
 }
---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _puts2 - show a table whit 9 multiplication
+ * @str:string to check
+ *
+ * Return: numbers (0 to 9)
+ */
+
+void puts2(char *str)
+{
+	int i;
+        while(str[i]!='\0')
+        {
+		if(i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+        }
+	_putchar('\n');
+}
