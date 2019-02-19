a8a1a48cda923d89bb9cb49b085a816e50bb0f9e
Yesid.dev <yesid.yag@gmail.com>
2019-02-19 14:36:51 +0000

    half of number, betty errors

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _puts2 - show a table whit 9 multiplication
+ * puts2 - show 1 char of two
  * @str:string to check
  *
  * Return: numbers (0 to 9)
@@ -10,9 +10,10 @@
 void puts2(char *str)
 {
 	int i;
-	while(str[i]!='\0')
+
+	while (str[i] != '\0')
 	{
-		if(i % 2 == 0)
+		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
