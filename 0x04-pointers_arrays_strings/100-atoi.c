bc528057130a594fb32c4cd3b8d64f05075492c7
Yesid.dev <yesid.yag@gmail.com>
2019-02-20 15:56:40 +0000

    correction

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -12,12 +12,12 @@ int _atoi(char *s)
 
 	if (s[i] != 0)
 	{
-		for (i = 0; (s[i] < '0' || s[i] > '9'); i++)
+		for (i = 0; (s[i] < 48 || s[i] > 57); i++)
 		{
-			if (s[i] == '-')
+			if (s[i] == 45)
 				neg = neg * -1;
 		}
-		for (; (s[i] >= '0' && s[i] <= '9'); i++)
+		for (; (s[i] >= 48 && s[i] <= 57); i++)
 		{
 			sum = sum * 10 - (s[i] - '0');
 		}
