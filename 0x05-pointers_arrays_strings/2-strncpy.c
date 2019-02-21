5b4f45aa960d4d40d4693e4169ea9815f6b74309
Yesid.dev <yesid.yag@gmail.com>
2019-02-21 17:39:49 +0000

    string over copy

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strcat - show a table whit 9 multiplication
+ * @dest: destination
+ * @src: string to add
+ * @n: number of lines
+ *
+ * Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+        for (i = 0; i < n && src[i] != '\0' ; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
