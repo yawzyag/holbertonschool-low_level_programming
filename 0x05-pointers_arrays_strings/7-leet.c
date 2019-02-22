4e017ef207cc58c0209c30811a8daa822a37ae52
Yesid <yesid.dev93@gmail.com>
2019-02-22 01:02:09 +0000

    program to change some values whit others, 1337

--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -1,12 +1,26 @@
 #include "holberton.h"
 
 /**
- * _isalpha - Check if a character is alphanumeric
- * @c: charachter to check
+ * leet - encode string
+ * @s: charachter to check
  *
- * Return: 1 if alpha 0 else
+ * Return: transformed text
  */
-char *leet(char *)
+char *leet(char *s)
 {
-	
+	int i, j;
+	char letters[] = "oOlLeEaAtT";
+	char num[] = "0011334477";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = num[j];
+			}
+		}
+	}
+	return(s);
 }
