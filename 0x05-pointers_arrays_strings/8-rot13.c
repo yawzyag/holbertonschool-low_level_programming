20a5710d1ba7f25c6eb8280197e03f36d7f47838
Yesid <yesid.dev93@gmail.com>
2019-02-22 01:56:35 +0000

    program to encrypt root 13

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * rot13 - encode string in root13
+ * @s: charachter to check
+ *
+ * Return: transformed textmoving 13 spaces
+ */
+char *rot13(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] >= 'a' && s[i] <= 'm')
+		    || (s[i] >= 'A' && s[i] <= 'M'))
+		{
+			s[i] = s[i] + 13;
+		}
+		else
+		{
+			while ((s[i] >= 'n' && s[i] <= 'z')
+			       || (s[i] >= 'N' && s[i] <= 'Z'))
+			{
+				s[i] = s[i] - 13;
+			}
+		}
+	}
+	return (s);
+}
