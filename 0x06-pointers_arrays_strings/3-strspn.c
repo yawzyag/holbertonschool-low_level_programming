b23cae6673483bdb5911e7e95c6627da70b93184
Yesid.dev <yesid.yag@gmail.com>
2019-02-25 15:40:41 +0000

    program to gets length of a prefix substring

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character to find
+ *
+ * Return: number acci code
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != c)
+		if (!*s++)
+			return 0;
+	return (s);
+}
+
+/**
+ * _strspn - gets length of a prefix substring
+ * @s: string to check
+ * @accept: coincidence
+ *
+ * Return: unsigned int coincidence
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	for(i = 0; *s && _strchr(accept,*s++); i ++)
+	{}
+	return (i);
+}
