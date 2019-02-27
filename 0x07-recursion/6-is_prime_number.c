b75527874a80259ec614b900db2078638b400abe
Yesid.dev <yesid.yag@gmail.com>
2019-02-27 23:13:58 +0000

    prime numbers 0 1

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+
+/**
+ * prime - show a table whit 9 multiplication
+ * @n: number
+ * @x: increment
+ *
+ * Return: char
+ */
+int prime(int n, int x)
+{
+	if (n > x)
+	{
+		if (n % x == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (prime(n, x + 1));
+		}
+	}
+}
+
+/**
+ * is_prime_number - print 0 or 1
+ * @n: num to check
+ *
+ * Return: 1 if prime 0 if not
+ */
+int is_prime_number(int n)
+{
+		int x = 2;
+
+		if (n < 2)
+		{
+			return (0);
+		}
+		else
+		{
+			return (prime(n, x) / n);
+		}
+}
