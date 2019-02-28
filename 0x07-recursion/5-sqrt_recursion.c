2199ceca01b4e9b8a869fff48c268b274b3f840f
Yesid <yesid.dev93@gmail.com>
2019-02-28 02:31:54 +0000

    root square of a natural number

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,6 +1,26 @@
 #include "holberton.h"
 
 /**
+ * _sqrt - natural square
+ * @n: string to print
+ * @x: string to count
+ *
+ * Return: num
+ */
+int _sqrt(int n, unsigned int x)
+{
+	if (x * x == n)
+	{
+		return (x);
+	}
+	if (x > n / 4)
+	{
+		return (-1);
+	}
+	return (_sqrt(n, ++x));
+}
+
+/**
  * _sqrt_recursion - print natural square
  * @n: string to print
  *
@@ -8,18 +28,7 @@
  */
 int _sqrt_recursion(int n)
 {
-	int x = n;
-	int y = 1;
-	while (x > y)
-	{
-		x = (x + y) / 2;
-		y = n / x;
-	}
-	if(x * x != n)
-	{   return -1;
-	}
-	else
-	{
-		return x;
-	}
+	unsigned int x = 0;
+
+	return (_sqrt(n, x));
 }
