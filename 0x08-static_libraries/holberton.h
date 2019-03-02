3191840dd0975d931e6f623e983b81e2fc7ccd85
Yesid <yesid.dev93@gmail.com>
2019-03-02 17:45:01 +0000

    holberton.h changes

--- 0x08-static_libraries/holberton.h
+++ 0x08-static_libraries/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 /* this prototype has to be declared in a header file */
 int _putchar(char c);
 int _islower(int c);
@@ -19,3 +22,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
