bf6c7ebafb645f2f0095a10c54c37804dd240b19
Yesid <yesid.dev93@gmail.com>
2019-03-02 16:14:21 +0000

    0-whatsmyname.c 1-args.c and holberton.h

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: comand count
+ * @argv: command passed
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
---
+++ 0x09-argc_argv/holberton.h
@@ -0,0 +1,2 @@
+/* this prototype has to be declared in holberton.h file */
+int _putchar(char c);
