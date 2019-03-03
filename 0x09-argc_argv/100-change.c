2006829b4cf503131fb7cfe8f99681a02e84a76f
Yesid <yesid.dev93@gmail.com>
2019-03-03 00:41:25 +0000

    betty corrections

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -4,7 +4,7 @@
 
 /**
  * centavo - convert to cent
- * @num: num to check
+ * @coin: num to check
  *
  * Return: num
  */
@@ -13,26 +13,26 @@ int centavo(int coin)
 	int cent1, cent2, cent3, cent4 = 0;
 	int num = coin;
 
-	if(num >= 25)
+	if (num >= 25)
 	{
 		cent1 = num / 25;
 		num = num % 25;
 	}
-	if(num >= 10)
-        {
+	if (num >= 10)
+	{
 		cent2 = num / 10;
 		num = num % 10;
-        }
-	if(num >= 5)
-        {
+	}
+	if (num >= 5)
+	{
 		cent3 = num / 5;
 		num = num % 5;
-        }
-	if(num >= 2)
-        {
+	}
+	if (num >= 2)
+	{
 		cent4 = num / 2;
 		num = num % 2;
-        }
+	}
 
 	return (cent1 + cent2 + cent3 + cent4 + num);
 }
@@ -47,16 +47,16 @@ int centavo(int coin)
 
 int main(int argc, char *argv[])
 {
-        if (argc != 2)
-        {
-                printf("Error\n");
-        }
-        else if (atoi(argv[1]) < 0)
-        {
-                printf("0\n");
-        }
-        else
-                printf("%d\n", centavo(atoi(argv[1])));
+	if (argc != 2)
+	{
+		printf("Error\n");
+	}
+	else if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+	}
+	else
+		printf("%d\n", centavo(atoi(argv[1])));
 
-        return (0);
+	return (0);
 }
