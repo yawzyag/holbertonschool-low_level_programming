43f1ae05e458aa0a873b138b19fcba591e2fb33b
Yesid.dev <yesid.yag@gmail.com>
2019-03-05 19:53:49 +0000

    changing errors

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -25,9 +25,9 @@ char *_strdup(char *str)
 		return (NULL);
 
 	for (i = 0; i < len; i++)
-		*(cpy + i) = *(str + i);
+		cpy[i] = str[i];
 
-	*(cpy + i) = '\0';
+	cpy[i] = '\0';
 
 	return (cpy);
 }
