958c5452680949d62acc42a119e3a630610278fe
Yesid.dev <yesid.yag@gmail.com>
2019-03-05 21:33:35 +0000

    addin changes

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -11,19 +11,23 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i;
+	int i, j;
 	int **arr;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = malloc( sizeof(int *) * width);
+	arr = malloc(sizeof(int *) * width);
+
+	for (i = 0; i < width; i++)
+		arr[i] = malloc(sizeof(int) * height);
 
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
-		arr[i] = malloc(sizeof(int) * height);
+	for (i = 0; i <  width; i++)
+		for (j = 0; j < height; j++)
+			arr[i][j] = 0;
 
 	return (arr);
 }
