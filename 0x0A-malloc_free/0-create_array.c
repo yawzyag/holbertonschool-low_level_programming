23a20327c7a28666b50a135eea99bd3813c56b5b
Yesid.dev <yesid.yag@gmail.com>
2019-03-05 15:20:30 +0000

    adding a program to create array of strings whit a specific char

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and with a specific char.
+ * @size: size
+ * @c: character
+ *
+ * Return: number acci code
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ptr;
+
+	if(size == 0)
+		return (0);
+
+	ptr = malloc(sizeof(*ptr) * size);
+
+	for (i = 0; i < size; i++)
+		*(ptr + i) = c;
+
+	return (ptr);
+}
---
+++ 0x0A-malloc_free/holberton.h
@@ -0,0 +1,4 @@
+/* this prototype has to be declared in holberton.h file */
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
