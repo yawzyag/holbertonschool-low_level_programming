cb9090ceaee6088809ef0bf6b024eae857e9d8b4
Yesid.dev <yesid.yag@gmail.com>
2019-03-05 21:45:07 +0000

    changes

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -17,16 +17,16 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = malloc(sizeof(int *) * width);
+	arr = malloc(sizeof(int *) * height);
 
 	for (i = 0; i < width; i++)
-		arr[i] = malloc(sizeof(int) * height);
+		arr[i] = malloc(sizeof(int) * width);
 
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i <  width; i++)
-		for (j = 0; j < height; j++)
+	for (i = 0; i <  height; i++)
+		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 
 	return (arr);
