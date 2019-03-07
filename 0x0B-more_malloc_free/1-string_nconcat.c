62aa3b48a0cd6e7bfdb53acfca6f5c62efb94a1e
Yesid.dev <yesid.yag@gmail.com>
2019-03-07 17:17:18 +0000

    betty error

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -27,31 +27,29 @@ int _strlen(char *s)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int len1, len2, len, i, j;
-        char *ptr;
+	char *ptr;
 
-        if (!s1)
+	if (!s1)
 		s1 = "";
 
 	if (!s2)
 		s2 = "";
 
-        len1 = _strlen(s1);
-        len2 = _strlen(s2);
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 
-        if (n <= len2)
-        {
+	if (n <= len2)
 		len2 = n;
-        }
 
-        len = len1 + len2;
-        ptr = malloc(sizeof(*ptr) * (len + 1));
-        if (ptr == NULL)
+	len = len1 + len2;
+
+	ptr = malloc(sizeof(*ptr) * (len + 1));
+	if (ptr == NULL)
 		return (0);
 
-        for (i = 0; s1[i]; i++)
-        {
+	for (i = 0; s1[i]; i++)
 		ptr[i] = s1[i];
-        }
+
 	for (j = 0; s2[j] && j < n; j++)
 	{
 		ptr[i] = s2[j];
