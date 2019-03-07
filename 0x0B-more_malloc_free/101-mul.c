f6424b18f3f9186269cf455c7466ec808d34d0b1
Yesid.dev <yesid.yag@gmail.com>
2019-03-07 22:00:01 +0000

    101

---
+++ 0x0B-more_malloc_free/101-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * main - multiplie two large numbers
+ * @argc: arg count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+}
