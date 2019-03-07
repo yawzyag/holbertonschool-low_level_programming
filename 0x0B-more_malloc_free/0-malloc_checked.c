a2c39da5f963fda322bf2411356568b7e97bcce5
Yesid.dev <yesid.yag@gmail.com>
2019-03-07 14:57:05 +0000

    function that allocates memory using malloc

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - creates an array of chars, and with a specific char.
+ * @b: number
+ *
+ * Return: number acci code
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *ptr;
+
+	ptr = (unsigned int *)malloc(b);
+
+	if (ptr == NULL)
+		exit (98);
+
+	return (ptr);
+}
---
+++ 0x0B-more_malloc_free/holberton.h
@@ -0,0 +1,2 @@
+/* this prototype has to be declared in holberton.h file */
+void *malloc_checked(unsigned int b);
