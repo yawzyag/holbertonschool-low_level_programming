d218593eef2ca6c9bc9d97d6d65f37c2fe7246e6
Yesid.dev <yesid.yag@gmail.com>
2019-03-07 17:00:28 +0000

    my own calloc function

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _memset - fills memory whit a constant byte
+ * @b: pointer to enter
+ * @c: constant byte for use
+ * @len: number of bytes
+ *
+ * Return: pointer to memory
+ */
+void  *_memset(void *b, int c, unsigned int len)
+{
+	unsigned char *p = b;
+
+	while (len > 0)
+	{
+		*p = c;
+		p++;
+		len--;
+	}
+
+	return (b);
+}
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: elements
+ * @size: size of type
+ *
+ * Return: allocate memory address
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int size_t;
+	void *p;
+
+	size_t = nmemb * size;
+
+	p = malloc(size_t);
+
+	if (!p)
+		return (NULL);
+
+	return (_memset(p, 0, size_t));
+}
--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -1,3 +1,5 @@
 /* this prototype has to be declared in holberton.h file */
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
