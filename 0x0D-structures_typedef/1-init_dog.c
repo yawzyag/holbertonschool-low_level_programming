fa770d212b3ad0e5ed1e3f2253c00e1ac199c356
Yesid.dev <yesid.yag@gmail.com>
2019-03-11 17:23:52 +0000

    malloc

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - Makes the sum of two numbers
  * @d: dog pointer to
@@ -11,7 +12,9 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	struct dog *ptr = d;
-	ptr -> name = name;
-	ptr -> age = age;
-	ptr -> owner = owner;
+
+	ptr = malloc(sizeof(*ptr));
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
 }
