39af26c7f6003f824939ff886420b3f5304535f1
Yesid.dev <yesid.yag@gmail.com>
2019-03-11 19:21:59 +0000

    1-init_dog.c

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -11,9 +11,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *ptr = d;
-
-	ptr->name = name;
-	ptr->age = age;
-	ptr->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
