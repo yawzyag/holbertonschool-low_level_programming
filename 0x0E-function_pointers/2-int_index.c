1f8bf668359a1934c78a42e1a8441fc591ad973a
Yesid.dev <yesid.yag@gmail.com>
2019-03-13 16:20:20 +0000

    2-int_index.c

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - show the index ocurrency
+ * @array: array to check
+ * @size: size of array
+ * @cmp: function to compare value
+ *
+ * Return: name of person coincidence
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if(size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if(cmp(array[i]))
+			return(i);
+	}
+	return (-1);
+}
