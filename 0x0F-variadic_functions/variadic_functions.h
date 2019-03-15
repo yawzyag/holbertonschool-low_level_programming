09a84d3fdf17c12b102d9306fb2cc891e5818c45
Yesid.dev <yesid.yag@gmail.com>
2019-03-15 17:17:20 +0000

    [Feature] Adding the library for the prototype functions

---
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdio.h>
+#include <stdarg.h>
+/* this prototype has to be declared in a header file */
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* FUNCTION_POINTERS_H */
