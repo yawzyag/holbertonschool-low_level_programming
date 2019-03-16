332414812ab023abf2083d688330dbcdd05b150e
Yesid <yesid.dev93@gmail.com>
2019-03-16 02:10:33 +0000

    adding changes

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,23 +1,20 @@
-section .text
+	;;  Define variables in the data section
+	SECTION .DATA
+hello:	     db 'Hello Holberton',10
+helloLen:	  equ $-hello
 
+	;;  Code goes in the text section
+	SECTION .TEXT
+	GLOBAL _start
 
 _start:
+	mov eax,4            	; 'write' system call = 4
+	mov ebx,1            	; file descriptor 1 = STDOUT
+	mov ecx,hello        	; string to write
+	mov edx,helloLen     	; length of string to write
+	int 80h              	; call the kernel
 
-	;  write our string to stdout.
-
-	mov     edx,len	; third argument: message length.
-	mov     ecx,msg	; second argument: pointer to message to write.
-	mov     ebx,1	; first argument: file handle (stdout).
-	mov     eax,4	; system call number (sys_write).
-	int     0x80	; call kernel.
-
-	;  and exit.
-
-	mov	ebx,0	  	; first syscall argument: exit code.
-	mov     eax,1	; system call number (sys_exit).
-	int     0x80	; call kernel.
-
-section .data
-
-msg     db      "Hello, world!",0xa ; the string to print.
-len     equ     $ - msg		    ; length of the string.
+	;;  Terminate program
+	mov eax,1            	; 'exit' system call
+	mov ebx,0            	; exit with error code 0
+	int 80h              	; call the kernel
