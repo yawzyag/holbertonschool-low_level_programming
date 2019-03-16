c0b5ab76e77d0a126a72393e0cf8c6ccd431fa75
Yesid.dev <yesid.yag@gmail.com>
2019-03-16 17:07:20 +0000

    [fix] problems

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -13,7 +13,7 @@ void print_all(const char * const format, ...)
 	va_list args;
 
 	va_start(args, format);
-	while (format[j] != '\0')
+	while (format && format[j] != '\0')
 	{
 		switch (format[j])
 		{
