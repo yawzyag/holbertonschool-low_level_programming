d077c76deeacacde088c54a7d32b717253a8bfbe
Yesid.dev <yesid.yag@gmail.com>
2019-03-21 20:43:09 +0000

    [Feature] free all

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - free nodes
+ *@head: node
+ *
+ * Return: 0
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		head->next = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
