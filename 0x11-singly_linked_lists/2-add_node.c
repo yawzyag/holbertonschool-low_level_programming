aee075b3daeeca9b8234ea0443480e77e2f684e1
Yesid.dev <yesid.yag@gmail.com>
2019-03-21 17:43:54 +0000

    add changes

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -27,7 +27,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node = (list_t *) malloc(sizeof(list_t));
 
 	if (!new_node)
-		return (0);
+		return (*head);
 	new_node->str = strdup(str);
 	new_node->len = _strlen(new_node->str);
 	new_node->next = *head;
