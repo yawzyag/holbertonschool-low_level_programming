b98276fb3746e7eb33a8726e67612651fc15f571
Yesid.dev <yesid.yag@gmail.com>
2019-03-21 21:29:13 +0000

    [Feature] print whitout main

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * fun - fun
+ *
+ * Return: number of nodes
+ */
+void fun(void) __attribute__((constructor));
+void fun(void)
+{
+printf("You're beat! and yet, you must allow");
+printf(",\nI bore my house upon my back!\n");
+}
