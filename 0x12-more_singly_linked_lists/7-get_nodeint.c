43bcfe2d8942ec91e5f5255d59a52bcf4e228a04
Yesid.dev <yesid.yag@gmail.com>
2019-03-25 15:55:52 +0000

    [Feature] get the nth node of a list of nodes

---
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	if(count == index)
+		return (head);
+	return (get_nodeint_at_index(head->next, index-1));
+}
