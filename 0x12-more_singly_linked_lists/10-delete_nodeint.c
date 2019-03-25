76575e6ae1fbaad6eda77917ea5b38027d1621c8
Yesid.dev <yesid.yag@gmail.com>
2019-03-25 17:57:51 +0000

    [Fix] betty errors

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(temp);
 		return (0);
 	}
-	for (i = 0; temp!=NULL && i < index-1; i++)
+	for (i = 0; temp != NULL && i < index - 1; i++)
 		temp = temp->next;
 
 	if (temp == NULL || temp->next == NULL)
