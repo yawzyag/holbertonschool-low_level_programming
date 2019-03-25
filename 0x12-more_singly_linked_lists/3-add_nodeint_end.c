0827376edd7fd111718d45548c06e833f12d636f
Yesid.dev <yesid.yag@gmail.com>
2019-03-25 14:49:24 +0000

    print node at the end

---
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add node at the end
+ * @head: node
+ * @n: string
+ *
+ * Return: number of nodes
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *reference;
+	listint_t *new_node = (listint_t *) malloc(sizeof(listint_t));
+
+	if (!new_node)
+		return (0);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (!*head)
+	{
+		*head = new_node;
+		return (*head);
+	}
+	reference = *head;
+	while (reference->next)
+		reference = reference->next;
+	reference->next = new_node;
+
+	return (new_node);
+}
