bc53ac83ef5e324c66e28da3e31ceb7655e49d55
Yesid.dev <yesid.yag@gmail.com>
2019-03-25 15:32:59 +0000

    [Feature] pop the first node

---
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - free head
+ *@head: node
+ *
+ * Return: num
+ */
+int pop_listint(listint_t **head)
+{
+	int num;
+	listint_t *next_node = NULL;
+
+	if (*head == NULL)
+		return (0);
+
+	next_node = (*head)->next;
+	num = (*head)->n;
+	free(*head);
+	*head = next_node;
+
+	return (num);
+}
