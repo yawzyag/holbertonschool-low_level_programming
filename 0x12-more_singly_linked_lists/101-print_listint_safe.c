d9e5da74f12e3113a37626fa570943291cbc9b40
Yesid <yesid.dev93@gmail.com>
2019-03-27 02:42:37 +0000

    [Feature] print safe version, whit loop inside

---
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - print even if have a loop
+ * @head: node head
+ * Return: count
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *slow, *fast;
+	int count, check;
+
+	if (!head)
+	{
+		exit(98);
+	}
+	slow = head;
+	count = 0;
+	while (slow)
+	{
+		fast = head;
+		check = 0;
+		while (count > check)
+		{
+			if (slow == fast)
+			{
+				printf("-> [%p] %d\n", (void *)slow, slow->n);
+				return (count);
+			}
+			fast = fast->next;
+			check++;
+		}
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		count++;
+	}
+	return (count);
+}
