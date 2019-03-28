a03254ee8ab6eaa2efc083a6dc37a77ab0ba7ce2
Yesid.dev <yesid.yag@gmail.com>
2019-03-28 15:06:41 +0000

    convert binary to unsigned int

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - binary to unsigned int
+ * @b: binary number
+ *
+ * Return: number in int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int val;
+
+	for (val = 0; *b; b++)
+	{
+		if (*b == '1')
+			val = (val << 1) | 1;
+		else if (*b == '0')
+			val <<= 1;
+		else
+			return (0);
+	}
+
+	return (val);
+}
---
+++ 0x13-bit_manipulation/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* this prototype has to be declared in a header file */
+unsigned int binary_to_uint(const char *b);
+#endif /* HOLBERTON_H */
