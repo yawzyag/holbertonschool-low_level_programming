6c7a977d49fc59d5fa34d19633992c5c4f4ee6f5
Yesid.dev <yesid.yag@gmail.com>
2019-03-28 17:27:03 +0000

    [Feature] print in binary base

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -6,4 +6,6 @@
 
 /* this prototype has to be declared in a header file */
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
 #endif /* HOLBERTON_H */
