9fb22352f43cc4a9239b5252759ba26a1e1e2ce9
Yesid.dev <yesid.yag@gmail.com>
2019-03-28 19:48:20 +0000

    [Fix] betty styke

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ * get_bit - get certain bit at index
+ * @n: num
+ * @index: index
+ *
+ * Return: num or -1
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index > 32)
