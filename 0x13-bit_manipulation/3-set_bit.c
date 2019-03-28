2b5560619baf93b852769145ea573ee2fb88a680
Yesid.dev <yesid.yag@gmail.com>
2019-03-28 20:15:37 +0000

    [Fix] max number

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -10,8 +10,9 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask = 1 << index;
+	unsigned long int uimax = ~0;
 
-	if (index > 64)
+	if (index > uimax)
 		return (-1);
 	*n = *n | mask;
 	return (1);
