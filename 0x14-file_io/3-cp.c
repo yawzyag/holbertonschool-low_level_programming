3a750273ebef2a594d2711856e1cc1311facb3d7
Yesid.dev <yesid.yag@gmail.com>
2019-04-02 23:20:16 +0000

    [Feature] weir file

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -19,6 +19,9 @@ int main(int ac, char **av)
 	{ dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98); }
 	fd_1 = open(av[1], O_RDONLY);
+	if (fd_1 == -1)
+	{ dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98); }
 	fd_2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_2 == -1)
 	{ dprintf(2, "Error: Can't write to %s\n", av[2]);
@@ -26,7 +29,9 @@ int main(int ac, char **av)
 	while (num_let == 1024)
 	{
 		num_let = read(fd_1, buf, sizeof(buf));
-
+		if (num_let == -1)
+		{ dprintf(2, "Error: Can't read from file %s\n", av[1]);
+			exit(98); }
 		num_let = write(fd_2, buf, num_let);
 		if (num_let == -1)
 		{ dprintf(2, "Usage: %s filename text\n", av[0]);
@@ -35,14 +40,10 @@ int main(int ac, char **av)
 	c_1 = close(fd_1);
 	c_2 = close(fd_2);
 	if (c_1 == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n'", fd_1);
-		exit(100);
-	}
+	{ dprintf(2, "Error: Can't close fd %d\n'", fd_1);
+		exit(100); }
 	if (c_2 == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n'", fd_2);
-		exit(100);
-	}
+	{dprintf(2, "Error: Can't close fd %d\n'", fd_2);
+		exit(100); }
 	return (0);
 }
