3b3439ed66d2f5126aa394377ee4228d317b5332
Yesid.dev <yesid.yag@gmail.com>
2019-04-02 23:55:35 +0000

    [fix] idk

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -13,34 +13,35 @@ int main(int ac, char **av)
 	char buf[1024];
 
 	if (ac != 3)
-	{ dprintf(2, "Usage: cp %s %s\n", av[1], av[2]);
+	{ dprintf(STDERR_FILENO, "Usage: cp %s %s\n", av[1], av[2]);
 		exit(97); }
 	fd_1 = open(av[1], O_RDONLY);
 	if (fd_1 == -1)
-	{ dprintf(2, "Error: Can't read from file %s\n", av[1]);
+	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98); }
 	fd_2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_2 == -1)
-	{ dprintf(2, "Error: Can't write to %s\n", av[2]);
+	{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99); }
 	while (num_let == 1024)
 	{
 		num_let = read(fd_1, buf, 1024);
 		if (num_let == -1)
-		{ dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		{ dprintf(STDERR_FILENO,
+			  "Error: Can't read from file %s\n", av[1]);
 			exit(98); }
 		num_let = write(fd_2, buf, num_let);
 		if (num_let == -1)
-		{ dprintf(2, "Error: Can't write to %s\n", av[2]);
+		{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99); }
 	}
 	c_1 = close(fd_1);
 	c_2 = close(fd_2);
 	if (c_1 == -1)
-	{ dprintf(2, "Error: Can't close fd %d\n", fd_1);
+	{ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_1);
 		exit(100); }
 	if (c_2 == -1)
-	{dprintf(2, "Error: Can't close fd %d\n", fd_2);
+	{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_2);
 		exit(100); }
 	return (0);
 }
