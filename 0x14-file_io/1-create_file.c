4555087d06eed09121d68640a59d60b2faddfe5c
Yesid.dev <yesid.yag@gmail.com>
2019-04-02 19:35:31 +0000

    [Fix] adding fix

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -29,10 +29,14 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
+
+	if (!text_content)
+		return(1);
 
 	num_letters = write(fd, text_content, _strlen(text_content));
 
