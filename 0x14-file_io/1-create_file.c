7ebbd911ed3e09cfe7aa07a945c77454f4bcf475
Yesid.dev <yesid.yag@gmail.com>
2019-04-02 18:02:58 +0000

    [Feature] Adding create file whit text

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+/**
+ * _strlen - show the length of string
+ *@s: string
+ *
+ * Return: lenght of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i);)
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - Read the file text
+ * @filename: name of the file
+ * @text_content: num of characters
+ *
+ * Return: number of letters
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, num_letters;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	num_letters = write(fd, text_content, _strlen(text_content));
+
+	if (num_letters == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
