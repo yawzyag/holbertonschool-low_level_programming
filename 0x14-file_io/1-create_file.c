dc01908d2e3947400a2c297c5cd2ab21b65efe4b
Yesid.dev <yesid.yag@gmail.com>
2019-04-02 19:40:21 +0000

    [Fix] betty erro

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -24,11 +24,10 @@ int _strlen(char *s)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, num_letters;
+	int fd, fd_num;
 
 	if (!filename)
 		return (-1);
-
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
@@ -36,11 +35,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-		return(1);
+		return (1);
 
-	num_letters = write(fd, text_content, _strlen(text_content));
+	fd_num = write(fd, text_content, _strlen(text_content));
 
-	if (num_letters == -1)
+	if (fd_num == -1)
 		return (-1);
 
 	close(fd);
