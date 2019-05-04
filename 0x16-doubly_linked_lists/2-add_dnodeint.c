ae904bc7649bbe75292ec5645af9877329b861cd
yawzyag <yesid.yag@gmail.com>
2019-05-04 17:02:54 +0000

    [Fix] head error

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -9,7 +9,7 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node, *aux;
+	dlistint_t *new_node;
 
 	if (!head)
 		return (0);
@@ -17,13 +17,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node = (dlistint_t *) malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (0);
-	aux = *head;
 	new_node->n = n;
-	new_node->next = aux;
+	new_node->next = *head;
 	new_node->prev = NULL;
-	if(aux)
-		aux->prev = new_node;
-	aux = new_node;
+	if (*head)
+		(*head)->prev = new_node;
+	*head = new_node;
 
 	return (new_node);
 }
