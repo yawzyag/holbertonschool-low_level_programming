5cfd59cf85ce2757ce67b7a1f1a0128c64d27122
yawzyag <yesid.yag@gmail.com>
2019-05-04 12:40:49 -0500

    [Fix] git add 7-insert_dnodeint.c git add 7-insert_dnodeint.c

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,7 +41,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current_node = current_node->next;
 		idx--;
 	}
-	if (current_node->next)
+	if (!current_node->next)
 		return (add_dnodeint_end(h, n));
 
 	new_node->next = current_node->next;
