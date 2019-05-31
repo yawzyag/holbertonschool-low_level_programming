bbf85bcf6e8f82bf2f5fc5706e7d11788756d3cf
Yesid.dev <yesid.yag@gmail.com>
2019-05-31 19:11:28 +0000

    [Fix] betty

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -9,22 +9,22 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    long unsigned int juanito;
-    hash_node_t *holi;
+	unsigned long int juanito;
+	hash_node_t *holi;
 
-    if (!ht)
-        return (NULL);
+	if (!ht)
+		return (NULL);
 
-    juanito = hash_djb2((unsigned char *)key) % ht->size;
-    holi = ht->array[juanito];
+	juanito = hash_djb2((unsigned char *)key) % ht->size;
+	holi = ht->array[juanito];
 
-    while (holi)
-    {
-        if (strcmp(holi->key, key) == 0)
-            break;
-        holi = holi->next;
-    }
-    if (!holi)
-        return (NULL);
-    return (holi->value);
+	while (holi)
+	{
+		if (strcmp(holi->key, key) == 0)
+			break;
+		holi = holi->next;
+	}
+	if (!holi)
+		return (NULL);
+	return (holi->value);
 }
