de37498a426c98ac9f4fc00bae9a98160fc8ecf4
Yesid.dev <yesid.yag@gmail.com>
2019-05-31 19:13:50 +0000

    [Fix] betty error

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -1,29 +1,29 @@
 #include "hash_tables.h"
 /**
- * hash_table_print - fills memory whit a constant byte
+ * hash_table_delete - fills memory whit a constant byte
  * @ht: pointer to enter
  *
  * Return: pointer to memory
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t *holi, *tmp;
-    unsigned long int i;
+	hash_node_t *holi, *tmp;
+	unsigned long int i;
 
-    if (!ht)
-        return;
-    for (i = 0; i < ht->size; i++)
-    {
-        tmp = ht->array[i];
-        while (tmp)
-        {
-            holi = tmp->next;
-            free(tmp->key);
-            free(tmp->value);
-            free(tmp);
-            tmp = holi;
-        }
-    }
-    free(ht->array);
-    free(ht);
+	if (!ht)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp)
+		{
+			holi = tmp->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			tmp = holi;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
