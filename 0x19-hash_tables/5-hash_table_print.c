a4e36eabafcd5f16fafe530b0e9fda15c14e5625
Yesid.dev <yesid.yag@gmail.com>
2019-05-31 19:12:45 +0000

    [Fix] betty indentation

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -8,25 +8,25 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *holi = NULL;
-    int num = 0;
+	unsigned long int i;
+	hash_node_t *holi = NULL;
+	int num = 0;
 
-    if (!ht)
-        return;
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        holi = ht->array[i];
+	if (!ht)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		holi = ht->array[i];
 
-        while (holi)
-        {
-            if (num)
-                printf(", ");
-            printf("'%s': '%s'", holi->key, holi->value);
-            num++;
-            holi = holi->next;
-        }
-    }
-    printf("}\n");
+		while (holi)
+		{
+			if (num)
+				printf(", ");
+			printf("'%s': '%s'", holi->key, holi->value);
+			num++;
+			holi = holi->next;
+		}
+	}
+	printf("}\n");
 }
