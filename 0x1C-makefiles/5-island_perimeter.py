a121a5b3f9f5056576f1a1210b25ca64b2cc0868
yawzyag <yesid.yag@gmail.com>
2019-07-05 23:20:26 +0000

    [fix] +4

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,33 +4,29 @@
 """
 
 
-def numofneighbour(mat, i, j, a1, a2):
-
-    count = 0
-
-    if (i > 0 and mat[i - 1][j]):
-        count += 1
-    if (j > 0 and mat[i][j - 1]):
-        count += 1
-
-    if (i < a1-1 and mat[i + 1][j]):
-        count += 1
-
-    if (j < a2-1 and mat[i][j + 1]):
-        count += 1
-
-    return count
-
-
 def island_perimeter(grid):
     """ holi """
 
-    array1 = len(grid)
-    array2 = len(grid[0])
+    a1 = len(grid)
+    a2 = len(grid[0])
+    count = 0
     perimeter = 0
-    for i in range(0, array1):
-        for j in range(0, array2):
-            if (grid[i][j]):
-                perimeter += (4 - numofneighbour(grid, i, j, array1, array2))
+    for i in range(a1):
+        for j in range(a2):
+            if (grid[i][j]) == 1:
+                if (i > 0 and grid[i - 1][j]):
+                    count += 1
+
+                if (j > 0 and grid[i][j - 1]):
+                    count += 1
+
+                if (i < a1-1 and grid[i + 1][j]):
+                    count += 1
+
+                if (j < a2-1 and grid[i][j + 1]):
+                    count += 1
+                if count != perimeter:
+                    perimeter = count
+                    perimeter += 4
 
     return perimeter
