9f454dcfa34b38a70483551ef55efa083018becc
yawzyag <yesid.yag@gmail.com>
2019-07-05 21:09:39 +0000

    [Fix] ignore no 0 water

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,16 +5,17 @@
 
 
 def island_perimeter(grid):
+    """ holi """
     numerito = 0
     for i in range(len(grid)):
         for j in range(len(grid)):
             if grid[i][j] == 1:
-                if grid[i][j - 1] == 0:
+                if i != 0 and grid[i][j - 1] == 0:
                     numerito += 1
-                if grid[i][j + 1] == 0:
+                if i != 0 and grid[i][j + 1] == 0:
                     numerito += 1
-                if grid[i - 1][j] == 0:
+                if j != 0 and grid[i - 1][j] == 0:
                     numerito += 1
-                if grid[i + 1][j] == 0:
+                if j != 0 and grid[i + 1][j] == 0:
                     numerito += 1
     return numerito
