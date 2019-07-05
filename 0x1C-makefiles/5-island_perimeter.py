87cc0bb29681e2c7fa56de2b95166fcafc608e32
yawzyag <yesid.yag@gmail.com>
2019-07-05 22:14:40 +0000

    [Fix] adding shit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,22 +2,66 @@
 """
 5-islita
 """
+R = 5
+C = 6
+
+
+def numofneighbour(mat, i, j):
+
+    count = 0
+
+    if (i > 0 and mat[i - 1][j]):
+        count += 1
+
+    if (j > 0 and mat[i][j - 1]):
+        count += 1
+
+    if (i < R-1 and mat[i + 1][j]):
+        count += 1
+
+    if (j < C-1 and mat[i][j + 1]):
+        count += 1
+
+    return count
+
+
+def findperimeter(mat):
+
+    perimeter = 0
+
+    for i in range(0, R):
+        for j in range(0, C):
+            if (mat[i][j]):
+                perimeter += (4 - numofneighbour(mat, i, j))
+
+    return perimeter
+
+
+def numofneighbour(mat, i, j):
+
+    count = 0
+
+    if (i > 0 and mat[i - 1][j]):
+        count += 1
+    if (j > 0 and mat[i][j - 1]):
+        count += 1
+
+    if (i < R-1 and mat[i + 1][j]):
+        count += 1
+
+    if (j < C-1 and mat[i][j + 1]):
+        count += 1
+
+    return count
 
 
 def island_perimeter(grid):
     """ holi """
-    numerito = 0
-    for i in range(len(grid)):
-        for j in range(len(grid)):
-            if grid[i][j] == 1:
-                if j != 0 and grid[i][j - 1] == 0:
-                    numerito += 1
-                if ((j + 1 < len(grid[i])) and
-                    grid[i][j + 1] == 0):
-                    numerito += 1
-                if i != 0 and grid[i - 1][j] == 0:
-                    numerito += 1
-                if (i + 1 < len(grid) and
-                    grid[i + 1][j] == 0):
-                    numerito += 1
-    return numerito
+
+    perimeter = 0
+    for i in range(0, R):
+        for j in range(0, C):
+            if (grid[i][j]):
+                perimeter += (4 - numofneighbour(grid, i, j))
+
+    return perimeter
