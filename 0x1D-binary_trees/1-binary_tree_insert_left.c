2997a59f4298ae07e513737813c5395acd785577
yawzyag <yesid.yag@gmail.com>
2019-07-24 20:26:58 +0000

    [Fix] new node

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -17,8 +17,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left)
 	{
 		parent->left->parent = new;
-		parent->left = new;
+		new->left = parent->left;
 	}
+	parent->left = new;
 
 	return (new);
 }
