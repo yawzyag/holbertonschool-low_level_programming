96e3263a752b534c9d2c9bd8b300fa8a9e054d4a
yawzyag <yesid.yag@gmail.com>
2019-07-24 21:52:11 +0000

    [Feature] adding advance

--- 0x1D-binary_trees/112-array_to_bst.c
+++ 0x1D-binary_trees/112-array_to_bst.c
@@ -1,11 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * array_to_bst -
- *
- * Return: 
+ * array_to_bst - array items to bst
+ * @array: array of num
+ * @size: size of array
+ * Return: root of the tree
  */
 bst_t *array_to_bst(int *array, size_t size)
 {
+	bst_t *root;
+	size_t i;
 
+	root = NULL;
+	if (!size)
+		return (root);
+	for (i = 0; i < size; i++)
+		bst_insert(&root, array[i]);
+
+	return (root);
 }
