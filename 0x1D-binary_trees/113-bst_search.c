50296996be2374f94f25c298c0bc0bf83d46af5e
yawzyag <yesid.yag@gmail.com>
2019-07-24 22:22:28 +0000

    [feature] function to search a value

--- 0x1D-binary_trees/113-bst_search.c
+++ 0x1D-binary_trees/113-bst_search.c
@@ -1,11 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * bst_search -
- *
- * Return: 
+ * bst_search - funtion to search vaue
+ * @tree: the root of the node
+ * @value: the value to search
+ * Return: return the node searched
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
+	if (!tree)
+		return (NULL);
 
+	if (tree->n == value)
+		return ((bst_t *)tree);
+	if (value < tree->n)
+		return (bst_search(tree->left, value));
+	else
+		return (bst_search(tree->right, value));
 }
