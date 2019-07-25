2593a0a6afc459a46adcc85932911f8a910a7696
yawzyag <yesid.yag@gmail.com>
2019-07-25 20:35:48 +0000

    [feature] func to remove

--- 0x1D-binary_trees/114-bst_remove.c
+++ 0x1D-binary_trees/114-bst_remove.c
@@ -1,11 +1,42 @@
 #include "binary_trees.h"
 
 /**
- * bst_remove -
- *
- * Return: 
+ * bst_remove - remove node
+ * @root: root of the tree
+ * @value: value to remove
+ * Return: tree root
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
+	bst_t *tmp, *succ;
 
+	if (!root)
+		return (root);
+
+	if (value < root->n)
+		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		root->right = bst_remove(root->right, value);
+	else
+	{
+		if (root->left != NULL && root->right != NULL)
+		{
+			succ = root->right;
+			while (succ->left)
+				succ = succ->left;
+			root->n = succ->n;
+			root->right = bst_remove(root->right, succ->n);
+		}
+		else
+		{
+			tmp = root;
+			if (root->left != NULL)
+				root = root->left;
+			else if (root->right != NULL)
+				root = root->right;
+			else
+				root = NULL;
+		}
+	}
+	return root;
 }
