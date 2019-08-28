8c27078ce088aad0d2c1d9b5de8fa80f9bdbb65e
yawzyag <yesid.yag@gmail.com>
2019-08-28 23:27:48 +0000

    [Fix] adding special case

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,17 +2,6 @@
 #include <math.h>
 
 /**
- * mini - function to find min
- * @x: array to traverse
- * @y: size of array
- * Return: min num
- */
-int mini(int x, int y)
-{
-	return (x < y ? x : y);
-}
-
-/**
  * jump_search - function to search in all items
  * @array: array to traverse
  * @size: size of array
@@ -21,31 +10,26 @@ int mini(int x, int y)
  */
 int jump_search(int *array, size_t size, int value)
 {
+	size_t i;
 	size_t jump = sqrt(size);
 	size_t left = 0;
 	size_t right = 0;
-	size_t i;
 
 	if (!array)
 		return (-1);
 	while (left < size && array[left] <= value)
 	{
-		right = mini(size - 1, left + jump);
+		right = left + jump;
 		printf("Value checked array[%lu] = [%d]\n", left, array[left]);
-		if (left + jump > size - 1)
-		{
-			left = right;
-			right += jump;
-			break;
-		}
-		if (array[left] <= value && array[right] >= value)
+
+		if ((array[left] <= value && array[right] >= value)
+			|| left + jump > size - 1)
 		{
 			break;
 		}
 		left += jump;
 	}
-		printf("Value found between indexes [%lu] and [%lu]\n", left, right);
-	left = mini(left, right);
+	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
 	for (i = left; i <= size - 1; ++i)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
